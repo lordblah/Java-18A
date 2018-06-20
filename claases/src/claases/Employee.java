c32a204089b2b1075aa675fe3515f5b41e245c26
Jonathan Hernandez <helharatheband@gmail.com>
2018-06-20 16:10:12 -0700

    classes practice

---
+++ claases/src/claases/Employee.java
@@ -0,0 +1,55 @@
+package claases;
+
+public class Employee 
+{
+	
+	//variable instances
+	private int empNum;
+	private String empLastName;
+	private String empFirstName;
+	private double empSalary;
+	//employee number getter
+	public int getEmpNum()
+	{
+		return empNum;
+	}
+	//employee number setter
+	public void setEmpNum(int emp)
+	{
+		empNum = emp;
+	}
+	//employee lastname getter
+	public String getEmpLAstName()
+	{
+		return empLastName;
+	}
+	//employee lastname setter
+	public void setEmpLastName(String name)
+	{
+		empLastName = name;
+	}
+	//employee first name getter
+	public String getEmpFirstName()
+	{
+		return empFirstName;
+	}
+	//employee first name setter
+	public void setEmpFirstName(String name)
+	{
+		empFirstName = name;
+	}
+	public double getEmpSalary()
+	{
+	return empSalary;
+	}
+	public void setEmpSalary(double sal)
+	{
+		empSalary = sal;
+	}
+	
+	public static void main(String[] args)
+	{
+		
+	}
+	
+}
