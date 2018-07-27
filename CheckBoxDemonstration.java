38f63245f78a5f3c6b84378c5130fea1c3fb0552
Jonathan Hernandez <helharatheband@gmail.com>
2018-07-26 20:36:29 -0700

    Update CheckBoxDemonstration.java

--- CheckBoxDemonstration.java
+++ CheckBoxDemonstration.java
@@ -1,40 +1,60 @@
-package realhomework5;
+package homework6;
+
 
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
-public class CheckBoxDemonstration extends JFrame implements ItemListener, ActionListener
+public class Jinsurance extends JFrame implements ItemListener, ActionListener
 {
    /**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 FlowLayout flow = new FlowLayout();
-   JLabel label = new JLabel("Insurance Plans");
+   JLabel label1 = new JLabel("Insurance Plans");
+   JLabel label2 = new JLabel("Vision Insurance "
+   		+ "price");
+   JLabel label3 = new JLabel("Dental Insurance Price");
+   JLabel label4 = new JLabel("PPO Plans Price");
+   JLabel label5 = new JLabel("HMO Plans Price");
+   JLabel label6 = new JLabel("Total");
    JCheckBox vision = new JCheckBox("vision insurance", false);
    JCheckBox dental = new JCheckBox("dental insurance", false);
    JButton button1 = new JButton("PPO");
    JButton button2 = new JButton("HMO");
-   JTextField totPrice = new JTextField(4);
+   JTextField visionPrice = new JTextField(14);
+   JTextField dentailPrice = new JTextField(14);
+   JTextField ppoPrice = new JTextField(14);
+   JTextField hmoPrice = new JTextField(14);
+   JTextField totPrice = new JTextField(5);
    int totalPrice = 0;
    int activebutton = 0;
    boolean firstButtonActived = false;
    boolean secondButtonActived = false;
-   public CheckBoxDemonstration()
+   public Jinsurance()
    {
       super("CheckBox Demonstration");
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       setLayout(new FlowLayout());
-      label.setFont(new Font("Arial", Font.ITALIC, 22));
+      label1.setFont(new Font("Arial", Font.ITALIC, 22));
       vision.addItemListener(this);
       dental.addItemListener(this);
       button1.addActionListener(this);
       button2.addActionListener(this);
-      add(label);
+      add(label1);
       add(vision);
       add(dental);
       add(button1);
       add(button2);
+      add(label2);
+      add(visionPrice);
+      add(label3);
+      add(dentailPrice);
+      add(label4);
+      add(ppoPrice);
+      add(label5);
+      add(hmoPrice);
+      add(label6);
       add(totPrice);
       
   }
@@ -48,84 +68,27 @@ FlowLayout flow = new FlowLayout();
 		   	
 			if(secondButtonActived == true)
 			{
+				
+				if(totalPrice > 95)totalPrice -= 200;
 				totalPrice += 600; 
-				totalPrice -= 200;
 				activebutton = 1;
 				secondButtonActived = false;
+				firstButtonActived = true;
+				ppoPrice.setText("$600.00");
+				hmoPrice.setText("$0.00");
 			}
 			else if(activebutton == 0 ) {
 				totalPrice += 600;
 				activebutton = 1;
 				firstButtonActived = true;
-			}
-			else if(activebutton == 1) 
-			{
-				totalPrice-= 600;
-		
-import java.awt.*;
-import javax.swing.*;
-import java.awt.event.*;
-public class CheckBoxDemonstration extends JFrame implements ItemListener, ActionListener
-{
-   /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-FlowLayout flow = new FlowLayout();
-   JLabel label = new JLabel("Insurance Plans");
-   JCheckBox vision = new JCheckBox("vision insurance", false);
-   JCheckBox dental = new JCheckBox("dental insurance", false);
-   JButton button1 = new JButton("PPO");
-   JButton button2 = new JButton("HMO");
-   JTextField totPrice = new JTextField(4);
-   int totalPrice = 0;
-   int activebutton = 0;
-   boolean firstButtonActived = false;
-   boolean secondButtonActived = false;
-   public CheckBoxDemonstration()
-   {
-      super("CheckBox Demonstration");
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      setLayout(new FlowLayout());
-      label.setFont(new Font("Arial", Font.ITALIC, 22));
-      vision.addItemListener(this);
-      dental.addItemListener(this);
-      button1.addActionListener(this);
-      button2.addActionListener(this);
-      add(label);
-      add(vision);
-      add(dental);
-      add(button1);
-      add(button2);
-      add(totPrice);
-      
-  }
-   
-   public void actionPerformed(ActionEvent actEvent)
-   {
-	   
-	   Object source = actEvent.getSource();
-	   if(source == button1)
-		  {
-		   	
-			if(secondButtonActived == true)
-			{
-				totalPrice += 600; 
-				totalPrice -= 200;
-				activebutton = 1;
-				secondButtonActived = false;
-        firstButtonActived = true;
-			}
-			else if(activebutton == 0 ) {
-				totalPrice += 600;
-				activebutton = 1;
-				firstButtonActived = true;
+				ppoPrice.setText("$600.00");
 			}
 			else if(activebutton == 1) 
 			{
 				totalPrice-= 600;
 				activebutton = 0; //means it already been subtracted
 				firstButtonActived = true;
+				ppoPrice.setText("$0.00");
 			}
 			
 			  
@@ -133,30 +96,35 @@ FlowLayout flow = new FlowLayout();
 		  }
 		  else if(source == button2)
 		  {
-			  if(firstButtonActived == true)
+			  if(firstButtonActived == true )
 				{
-					totalPrice -= 600;
+					if(totalPrice > 95)totalPrice -= 600;
 					totalPrice += 200;
 					activebutton = 1;
 					firstButtonActived = false;
-          secondButtonActived = true;
+					secondButtonActived = true;
+					hmoPrice.setText("$200.00");
+					ppoPrice.setText("$0.00");
 				}
 				else if(activebutton == 0 ) {
 					totalPrice += 200;
 					activebutton = 1;
 					secondButtonActived = true;
+					hmoPrice.setText("$200.00");
 				}
 				else if(activebutton == 1) 
 				{
 					totalPrice-= 200;
 					activebutton = 0; //means it already been subtracted
 					secondButtonActived = true;
+					hmoPrice.setText("$0.00");
 				
 				}
 			  
 			  totPrice.setText("$" + totalPrice);
 		  }
    }
+
    @Override
   public void itemStateChanged(ItemEvent event)
   {
@@ -170,11 +138,13 @@ FlowLayout flow = new FlowLayout();
 		  {
 			  totalPrice += 20;
 			  totPrice.setText("$" + totalPrice);
+			  visionPrice.setText("$20.00");
 		  }
 		  else
 		  {
 			  totalPrice -= 20;
 			  totPrice.setText("$" + totalPrice);
+			  visionPrice.setText("$0.00");
 		  }
 	  }
 	  else if(source == dental)
@@ -183,90 +153,28 @@ FlowLayout flow = new FlowLayout();
 			  {
 			  totalPrice += 75;
 			  totPrice.setText("$" + totalPrice);
+			  dentailPrice.setText("$75.00");
 			  }else {
-		  totalPrice -= 75;
-		  totPrice.setText("$" + totalPrice);
-		  }
+			  totalPrice -= 75;
+			  totPrice.setText("$" + totalPrice);
+			  dentailPrice.setText("$0.00");
+			  }
 	
 		  }
 	  
 	}
-} 
+
   
-		activebutton = 0; //means it already been subtracted
-				firstButtonActived = true;
-			}
-			
-			  
-			  totPrice.setText("$" + totalPrice);
-		  }
-		  else if(source == button2)
-		  {
-			  if(firstButtonActived == true)
-				{
-					totalPrice -= 600;
-					totalPrice += 200;
-					activebutton = 1;
-					firstButtonActived = false;
-				}
-				else if(activebutton == 0 ) {
-					totalPrice += 200;
-					activebutton = 1;
-					secondButtonActived = true;
-				}
-				else if(activebutton == 1) 
-				{
-					totalPrice-= 200;
-					activebutton = 0; //means it already been subtracted
-					secondButtonActived = true;
-				
-				}
-			  
-			  totPrice.setText("$" + totalPrice);
-		  }
-   }
-   @Override
-  public void itemStateChanged(ItemEvent event)
-  {
-      // Actions based on choice go here
-	  Object source = event.getSource();
-	  int select = event.getStateChange();
-	  
-	  if(source == vision)
-	  {
-		  if(select == ItemEvent.SELECTED)
-		  {
-			  totalPrice += 20;
-			  totPrice.setText("$" + totalPrice);
-		  }
-		  else
-		  {
-			  totalPrice -= 20;
-			  totPrice.setText("$" + totalPrice);
-		  }
-	  }
-	  else if(source == dental)
-		  {
-		  if(select == ItemEvent.SELECTED)
-			  {
-			  totalPrice += 75;
-			  totPrice.setText("$" + totalPrice);
-			  }else {
-		  totalPrice -= 75;
-		  totPrice.setText("$" + totalPrice);
-		  }
-	
-		  }
-	  
-	}
+		
 	  
   public static void main(String[] arguments)
   {
       final int FRAME_WIDTH = 190;
-      final int FRAME_HEIGHT = 320;
-      CheckBoxDemonstration frame =
-         new CheckBoxDemonstration();
+      final int FRAME_HEIGHT = 420;
+      Jinsurance frame =
+         new Jinsurance();
       frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
       frame.setVisible(true);
   }
 }
+
